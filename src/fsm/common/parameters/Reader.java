5655e6591a99f928c7fdf4c54ed9925159c0798c
Kurt.Hagen <fivestarmoon@live.ca>
2018-01-13 11:20:21 -0800

    Redesign to remove the array hack-like implementation

---
+++ src/fsm/common/parameters/Reader.java
@@ -0,0 +1,218 @@
+package fsm.common.parameters;
+
+import java.util.Set;
+
+import fsm.common.Log;
+import fsm.common.parameters.InternalDef.BooleanArray;
+import fsm.common.parameters.InternalDef.DoubleArray;
+import fsm.common.parameters.InternalDef.LongArray;
+import fsm.common.parameters.InternalDef.StringArray;
+import fsm.common.parameters.InternalDef.Struct;
+import fsm.common.parameters.InternalDef.StructArray;
+
+public class Reader
+{
+   public Reader struct(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || object instanceof Struct )
+      {
+         currStruct_ = (Struct) object;
+      }
+      else
+      {
+         Log.severe("Failed to enter struct [" + key + "]");
+      }
+      return this;      
+   }
+   
+   public Reader[] structArray(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object != null && object instanceof StructArray )
+      {
+         StructArray array = (StructArray) object;
+         Reader[] readers = new Reader[array.array_.length];
+         for ( int ii=0; ii<readers.length; ii++ )
+         {
+            readers[ii] = new Reader(array.array_[ii]);
+         }
+         return readers;
+      }
+      else
+      {
+         Log.severe("Failed to enter structarray [" + key + "]");
+         return new Reader[0];
+      }   
+   }
+   
+   public String[] getKeySet() 
+   {
+      Set<String> set = currStruct_.struct.keySet();
+      return set.toArray(new String[set.size()]);
+   }
+   
+   public boolean isKeyForStruct(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object != null && object instanceof Struct )
+      {
+         return true;
+      }
+      return false;
+   }
+   
+   public boolean isKeyForArrayOfValues(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object != null 
+               && ( object instanceof StringArray
+                    || object instanceof BooleanArray 
+                    || object instanceof LongArray
+                    || object instanceof DoubleArray) )
+      {
+         return true;
+      }
+      return false;
+   }
+   
+   public boolean isKeyForArrayOfStructs(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object != null && object instanceof StructArray )
+      {
+         return true;
+      }
+      return false;
+   }
+   
+   public boolean isKeyForValue(String key)
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object != null 
+               && ( object instanceof String
+                    || object instanceof Boolean 
+                    || object instanceof Long
+                    || object instanceof Double) )
+      {
+         return true;
+      }
+      return false;
+   }
+   
+   public String getStringValue(String key, String defaultValue) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof String) )
+      {
+         Log.severe("Failed to get string value for [" + key + "]");
+         return defaultValue;
+      }
+      return (String) object;
+   }
+   
+   public boolean getBooleanValue(String key, boolean defaultValue) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof Boolean) )
+      {
+         Log.severe("Failed to get boolean value for [" + key + "]");
+         return defaultValue;
+      }
+      return (Boolean) object;
+   }
+   
+   public long getLongValue(String key, long defaultValue) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof Long) )
+      {
+         Log.severe("Failed to get long value for [" + key + "]");
+         return defaultValue;
+      }
+      return (Long) object;
+   }
+   
+   public double getDoubleValue(String key, double defaultValue) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof Double) )
+      {
+         Log.severe("Failed to get double value for [" + key + "]");
+         return defaultValue;
+      }
+      return (Double) object;
+   }
+   
+   public String[] getStringArray(String key) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof StringArray) )
+      {
+         Log.severe("Failed to get string array for [" + key + "]");
+         return new String[0];
+      }
+      else
+      {
+         StringArray array = (StringArray) object;
+         return array.array_.clone();
+      }
+   }
+   
+   public boolean[] getBooleanArray(String key) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof BooleanArray) )
+      {
+         Log.severe("Failed to get boolean array for [" + key + "]");
+         return new boolean[0];
+      }
+      else
+      {
+         BooleanArray array = (BooleanArray) object;
+         return array.array_.clone();
+      }
+   }
+   
+   public long[] getLongArray(String key) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof LongArray) )
+      {
+         Log.severe("Failed to get long array for [" + key + "]");
+         return new long[0];
+      }
+      else
+      {
+         LongArray array = (LongArray) object;
+         return array.array_.clone();
+      }
+   }
+   
+   public double[] getDoubleArray(String key) 
+   {
+      Object object = currStruct_.struct.get(key);
+      if ( object == null || !(object instanceof DoubleArray) )
+      {
+         Log.severe("Failed to get double array for [" + key + "]");
+         return new double[0];
+      }
+      else
+      {
+         DoubleArray array = (DoubleArray) object;
+         return array.array_.clone();
+      }
+   }
+   
+   // --- PACKAGE PROTECTED
+   
+   Reader(Struct struct)
+   {
+      currStruct_ = struct;
+   }
+   
+   // --- PRIVATE
+   
+   private Struct currStruct_;
+
+}
--- test/fsm/common/jsonParameters/TestParameters.java
+++ test/fsm/common/jsonParameters/TestParameters.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import fsm.common.Log;
+import fsm.common.parameters.Parameters;
+import fsm.common.parameters.Reader;
+import fsm.common.parameters.Writer;
 
 class TestParameters
 {
@@ -41,16 +44,12 @@ class TestParameters
       writer.setValue("string", "string2");
       writer.setArrayOfValues("doublearray", new double[]{ 0.1, 0.2 });
       
-      writer = params.getWriter();
-      writer.allocateArrayOfStructs("structarray", 2);
-      
-      writer = params.getWriter().struct(Parameters.GetKeyForArrayIndex("structarray", 0));
-      writer.setValue("long", 3);
-      writer.setValue("double", 2.1);
-      
-      writer = params.getWriter().struct(Parameters.GetKeyForArrayIndex("structarray", 1));
-      writer.setValue("boolean", true);
-      writer.setArrayOfValues("stringarray", new String[]{ "a", "b" });
+      Writer[] arraywriters = params.getWriter().allocateArrayOfStructs(2);      
+      arraywriters[0].setValue("long", 3);
+      arraywriters[0].setValue("double", 2.1);      
+      arraywriters[1].setValue("boolean", true);
+      arraywriters[1].setArrayOfValues("stringarray", new String[]{ "a", "b" });
+      params.getWriter().setArrayOfStructs("structarray", arraywriters);
       
       params.write();
       System.out.println("  write completed.");
@@ -68,58 +67,54 @@ class TestParameters
       assertEquals(true, params.getReader().isKeyForValue("long"));
       assertEquals(false, params.getReader().isKeyForArrayOfValues("long"));
       assertEquals(false, params.getReader().isKeyForStruct("long"));
-      assertEquals(1, params.getReader().getValue("long", 0));
+      assertEquals(1, params.getReader().getLongValue("long", 0));
 
       assertEquals(true, params.getReader().isKeyForValue("double"));
-      assertEquals(3.1456789124, params.getReader().getValue("double", 0.0));
+      assertEquals(3.1456789124, params.getReader().getDoubleValue("double", 0.0));
 
       assertEquals(true, params.getReader().isKeyForValue("boolean"));
-      assertEquals(true, params.getReader().getValue("boolean", false));
+      assertEquals(true, params.getReader().getBooleanValue("boolean", false));
 
       assertEquals(true, params.getReader().isKeyForValue("string"));
-      assertEquals("string", params.getReader().getValue("string", ""));
+      assertEquals("string", params.getReader().getStringValue("string", ""));
 
       assertEquals(true, params.getReader().isKeyForArrayOfValues("intarray"));
       assertEquals(false, params.getReader().isKeyForArrayOfStructs("intarray"));
       assertEquals(false, params.getReader().isKeyForValue("intarray"));
       assertEquals(false, params.getReader().isKeyForStruct("intarray"));
-      assertEquals(3, params.getReader().getArrayLength("intarray"));
-      assertEquals(0, params.getReader().getValue(Parameters.GetKeyForArrayIndex("intarray", 0), -1));
-      assertEquals(1, params.getReader().getValue(Parameters.GetKeyForArrayIndex("intarray", 1), -1));
-      assertEquals(2, params.getReader().getValue(Parameters.GetKeyForArrayIndex("intarray", 2), -1));
+      long[] intarray = params.getReader().getLongArray("intarray");
+      assertEquals(3, intarray.length);
+      assertEquals(0, intarray[0]);
+      assertEquals(1, intarray[1]);
+      assertEquals(2, intarray[2]);
 
       assertEquals(true, params.getReader().isKeyForArrayOfValues("boolarray"));
-      assertEquals(2, params.getReader().getArrayLength("boolarray"));
-      assertEquals(true, params.getReader().getValue(Parameters.GetKeyForArrayIndex("boolarray", 0), false));
-      assertEquals(false, params.getReader().getValue(Parameters.GetKeyForArrayIndex("boolarray", 1), true));
-
-      assertEquals(true, params.getReader().isKeyForArrayOfValues("boolarray"));
-      assertEquals(2, params.getReader().getArrayLength("boolarray"));
-      assertEquals(true, params.getReader().getValue(Parameters.GetKeyForArrayIndex("boolarray", 0), false));
-      assertEquals(false, params.getReader().getValue(Parameters.GetKeyForArrayIndex("boolarray", 1), true));
+      assertEquals(2, params.getReader().getBooleanArray("boolarray").length);
+      assertEquals(true, params.getReader().getBooleanArray("boolarray")[0]);
+      assertEquals(false, params.getReader().getBooleanArray("boolarray")[1]);
 
       assertEquals(true, params.getReader().isKeyForStruct("struct"));
       assertEquals(false, params.getReader().isKeyForValue("struct"));
       assertEquals(false, params.getReader().isKeyForArrayOfValues("struct"));
       Reader structReader = params.getReader().struct("struct");
-      assertEquals("string2", structReader.getValue("string", ""));
-      assertEquals(2, structReader.getArrayLength("doublearray"));
-      assertEquals(0.1, structReader.getValue(Parameters.GetKeyForArrayIndex("doublearray", 0), -1.0));
-      assertEquals(0.2, structReader.getValue(Parameters.GetKeyForArrayIndex("doublearray", 1), -1.0));
+      assertEquals("string2", structReader.getStringValue("string", ""));
+      assertEquals(2, structReader.getDoubleArray("doublearray").length);
+      assertEquals(0.1, structReader.getDoubleArray("doublearray")[0]);
+      assertEquals(0.2, structReader.getDoubleArray("doublearray")[1]);
 
       assertEquals(true, params.getReader().isKeyForArrayOfStructs("structarray"));
       assertEquals(false, params.getReader().isKeyForArrayOfValues("structarray"));
-      assertEquals(2,  params.getReader().getArrayLength("structarray"));
+      assertEquals(2,  params.getReader().structArray("structarray").length);
       
-      structReader = params.getReader().struct(Parameters.GetKeyForArrayIndex("structarray", 0));
-      assertEquals(3, structReader.getValue("long", 0));
-      assertEquals(2.1, structReader.getValue("double", 0.0));
+      structReader = params.getReader().structArray("structarray")[0];
+      assertEquals(3, structReader.getLongValue("long", 0));
+      assertEquals(2.1, structReader.getDoubleValue("double", 0.0));
       
-      structReader = params.getReader().struct(Parameters.GetKeyForArrayIndex("structarray", 1));
-      assertEquals(true, structReader.getValue("boolean", false));
-      assertEquals(2, structReader.getArrayLength("stringarray"));
-      assertEquals("a", structReader.getValue(Parameters.GetKeyForArrayIndex("stringarray", 0), ""));
-      assertEquals("b", structReader.getValue(Parameters.GetKeyForArrayIndex("stringarray", 1), ""));
+      structReader = params.getReader().structArray("structarray")[1];
+      assertEquals(true, structReader.getBooleanValue("boolean", false));
+      assertEquals(2, structReader.getStringArray("stringarray").length);
+      assertEquals("a", structReader.getStringArray("stringarray")[0]);
+      assertEquals("b", structReader.getStringArray("stringarray")[1]);
       System.out.println("  validating completed.");
    }
 
